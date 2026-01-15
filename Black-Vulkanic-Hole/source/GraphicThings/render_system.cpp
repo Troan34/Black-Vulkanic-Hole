3c0c8c27aa7f23f845537ddb20a7df365af287f4
Troan34 <roanbukaci5@gmail.com>
2026-01-15 19:23:05 +0100

    Chore: 12th episode of Galea's tutorial

--- Black-Vulkanic-Hole/source/GraphicThings/render_system.cpp
+++ Black-Vulkanic-Hole/source/GraphicThings/render_system.cpp
@@ -48,12 +48,12 @@ namespace engine
 
 		for (auto& obj : gameObjects)
 		{
-			obj.transform2D.rotation = glm::mod(obj.transform2D.rotation + .01f, glm::two_pi<float>());
+			obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + .02f, glm::two_pi<float>());
+			obj.transform.rotation.x = glm::mod(obj.transform.rotation.x + .01f, glm::two_pi<float>());
 
 			SimplePushConstantData push{};
-			push.offset = obj.transform2D.translation;
 			push.color = obj.color;
-			push.transform = obj.transform2D.mat2();
+			push.transform = obj.transform.mat4();
 
 			vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
 
